a02a2043967b2798d5931d510fa4c21642a6b74c
HeineKayn <thomashoger@hotmail.fr>
2021-11-09 13:18:35 +0100

    Update scrapItem.py

--- scrap/scrapItem.py
+++ scrap/scrapItem.py
@@ -5,7 +5,7 @@ def run(cur=None):
 	item_page_content = Scrap(item_url).find("div",{"id":"mw-content-text"})
 
 	item_tables = item_page_content.findAll("div",{"class":"tlist"})
-	item_table_dic = {"legendaires":item_tables[7],"mythiques":item_tables[8],"bottes":item_tables[4]}
+	item_table_dic = {"bottes":item_tables[4],"legendaires":item_tables[7],"mythiques":item_tables[8]}
 	item_dic = {}
 
 	for key,item_table in item_table_dic.items() :
@@ -17,11 +17,22 @@ def run(cur=None):
 			name = name.replace(" item.png","")
 			icon = item_container["src"]
 
-			item_list += [{"name":name,"icon":icon}]
+			item_list += [name]
+			# item_list += [{"name":name,"icon":icon}]
+
+			if cur :
+				if key=="bottes":
+					Q_Botte = "INSERT INTO Botte (nom) VALUES (%s)"
+					cur.execute(Q_Botte,(name,))
+				if key=="legendaires":
+					Q_Item_Update = "INSERT INTO Interm_Item (winrate, pickrate, championID, laneID, itemID) VALUES (%s,%s,(SELECT ID FROM Champion WHERE nom = %s),(SELECT ID FROM Lane WHERE nom = %s),(SELECT ID FROM Item WHERE nom = %s)) ON DUPLICATE KEY UPDATE winrate = VALUES(winrate), pickrate = VALUES(pickrate), championID = VALUES(championID), LaneID = VALUES(LaneID), ItemID = VALUES(ItemID)"
+					cur.execute(Q_Item_Update,(winrate,pickrate,champ_name,role,name))
+				if key=="mythiques":
+					pass
 
 		item_dic[key] = item_list
-		
-	print(item_dic['bottes'])
+
+	print(item_dic['legendaires'])
 
 if __name__ == "__main__" :
 	run()
