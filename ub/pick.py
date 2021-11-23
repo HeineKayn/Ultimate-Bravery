8fae5d6c629551eaf37472ea2de76de7e96a36cd
HeineKayn <thomashoger@hotmail.fr>
2021-11-23 22:47:34 +0100

    p

--- ub/pick.py
+++ ub/pick.py
@@ -10,17 +10,17 @@ class Picker():
 	    dic = {}
 	    dic["titre"] = pickTitre.run()
 
-	    dic["champion_name"] = pickChampion.run([],bdd)
+	    dic["champion_name"] = pickChampion.run([],self.bdd)
 	    champ_name           = dic["champion_name"]["name"]
 
-	    dic["lane"] = pickLane.run(difficulte,carte,champ_name,bdd)
+	    dic["lane"] = pickLane.run(difficulte,carte,champ_name,self.bdd)
 	    lane        = dic["lane"]
 
-	    dic["ordre"] = pickSkill.run(difficulte,champ_name,bdd)
+	    dic["spell"] = pickSpell.run(difficulte,carte,self.bdd)
+	    dic["ordre"] = pickSkill.run(difficulte,champ_name,self.bdd)
 
 	    # dic["item"]  = Pick_Item(difficulte, carte, lane, champ_name) 
 	    # dic["item"] += Pick_Botte(difficulte, carte, lane, champ_name)
-	    # dic["summ"]  = Pick_Summoners(difficulte, lane, carte)
 	    # dic["rune"]  = Pick_Rune(difficulte, lane, champ_name)
 
 	    return dic
@@ -32,8 +32,10 @@ picker = Picker(bdd)
 x = pickTitre.run()
 x = pickChampion.run([],bdd)
 x = pickSpell.run(5,"classic",bdd)
+x = pickSkill.run(1,"vi",bdd)
+
 # x = pickLane.run(1,"classic","vi",bdd)
-# x = pickSkill.run(1,"vi",bdd)
+
 
 
 print(x)
